bdf62d8884c82f024b9be7c5d77844d816cec68a
Maximo <chiesakarkovichm@gmail.com>
2025-08-04 18:22:51 -0300

    Creacion crud version_circuito.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import { categoryRouter } from "./category/category.routes.js";
 import { orm, syncSchema } from './shared/orm.js';
 import { RequestContext } from '@mikro-orm/core';
+import { Circuit_VersionController as controller } from "./circuit_version/circuit_version.controller.js"; 
+import { circuit_versionRouter } from "./circuit_version/circuit_version.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -15,6 +17,7 @@ app.use((req, res, next) => {
 //antes de las rutas y de los middleware de negocio
 
 app.use("/api/categories", categoryRouter);
+app.use("/api/circuit_versions", circuit_versionRouter);
 
 
 app.use((_, res) => {
