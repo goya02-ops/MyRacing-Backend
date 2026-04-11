af6de973fea797e8641f26b14fb81b5313adbb34
Luciano Marini <luchomarini2002@gmail.com>
2026-04-11 13:44:58 -0300

    Adding reset and forgot routes

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -7,3 +7,5 @@ authRouter.post('/register', AuthController.register);
 authRouter.post('/login', AuthController.login);
 authRouter.post('/refresh', AuthController.refreshAccessToken);
 authRouter.post('/logout', AuthController.logout);
+authRouter.post('/forgot-password', AuthController.forgotPassword);
+authRouter.post('/reset-password', AuthController.resetPassword);
